d07527f64fd044df1db7a41a5b2c0d84af5431ef
poojagit912 <36732363+poojagit912@users.noreply.github.com>
2020-09-08 21:09:02 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "heroku" {
+     email   = "jilani.oracledba@gmail.com"
+     api_key = "7353f36e-9487-4499-b846-7d948a655867"
+}
+
+resource "heroku_app" "example" {
+  name   = "test-my-app"
+  region = "us"
+}
+
+resource "heroku_build" "example" {
+  app = "my-name"
+    
+  source = {
+    path = "target/hello.jar"
+  }
+    provisioner "local-exec" {
+       command= "heroku deploy:jar target/hello.jar --app sushi"     
+}
+}
